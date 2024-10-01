c47686e2fa09e1cbbd59c1eb803c017b57113729
kuuuuumiiiii <kuuuuumiiiii@users.noreply.github.com>
2024-10-02 01:23:58 +0900

    リロード対応

--- react/react_todo_list/src/App.js
+++ react/react_todo_list/src/App.js
@@ -1,12 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { InputForm } from './components/InputForm';
 import { Title } from './components/Title';
 import { TodoList } from './components/TodoList';
 
 function App() {
-  const [taskList, setTaskList] = useState([]);
+  // 初期状態としてlocalStorageからデータを取得
+  const [taskList, setTaskList] = useState(() => {
+    const savedTasks = localStorage.getItem('taskList');
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
 
+  // taskListが変更されるたびにlocalStorageに保存
+  useEffect(() => {
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+  }, [taskList]);
+  
   return (
     <div className="body">
       <Title />
--- react/react_todo_list/src/components/TodoList.jsx
+++ react/react_todo_list/src/components/TodoList.jsx
@@ -7,16 +7,31 @@ export const TodoList = ({taskList, setTaskList}) => {
     setTaskList(taskList.filter((task) => task.id !== id));
   }
 
+  const handleCompleted = (id) => {
+    // 完了か未完了かを判断する
+    const updatedTaskList = taskList.map((task) => {
+      if (id === task.id) {
+        return {
+          ...task,
+          completed: !task.completed,
+        };
+      }
+      return task;
+    });
+    setTaskList(updatedTaskList);
+    localStorage.setItem('taskList', JSON.stringify(updatedTaskList));
+  };
+
   return (
     <div className="todoList">
       <div className="todos">
         {taskList.map((task, index) => (
-          <div className="todo" key={index}>
+          <div className={`todo${task.completed ? "completed" : ""}`} key={index}>
           <div className="todoText">
             <span>{task.text}</span>
           </div>
           <div className="icons">
-            <input type="checkbox" />
+            <input type="checkbox" checked={task.completed} onClick={() => handleCompleted(task.id)}/>
             <button className="edit">編集</button>
             <button onClick={() => handleDelete(task.id)} className="delete">削除</button>
           </div>
